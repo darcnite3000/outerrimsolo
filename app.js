899d37002841112d664d454b495d21b9d4ed524b
Tristan Smith <darcnite3000@gmail.com>
2019-11-09 00:57:08 +1100

    add basic styling

--- app.js
+++ app.js
@@ -28,48 +28,67 @@ function loadDeck(id, { sections, automa }) {
     const card = document.getElementById('card')
     card.innerHTML = ''
     sections.forEach(({ title, id, description, ordered }) => {
+      let innerHTML = ''
       if (drawnCard[id].length) {
-        card.innerHTML += `<div id="card-${id}"><div class="title"><b>${title}</b>`
+        innerHTML += `<div class="card-section ${id}"><div class="card-section-blurb"><span class="card-section-title">${title}</span>`
         if (description) {
-          card.innerHTML += ` - <span>${description}</span></div>`
+          innerHTML += ` - <span class="card-section-description">${description}</span>`
         }
-        card.innerHTML += `<${ordered ? 'ol' : 'ul'}>${drawnCard[id]
+        innerHTML += `</div><${
+          ordered ? 'ol' : 'ul'
+        } class="card-section-actions">${drawnCard[id]
           .map(s => `<li>${s}</li>`)
           .join('')}</${ordered ? 'ol' : 'ul'}></div>`
       }
+      card.innerHTML += innerHTML
     })
-    card.innerHTML += `<div id="card-number">${drawnCard.card}</div>`
+    card.innerHTML += `<div class="card-number">${drawnCard.card}</div>`
 
     if (drawnCard.shuffle) {
       deck = shuffleDeck(deck)
     }
   }
-  app.innerHTML = `<section class="deck ${ai.id}"><header>${
+  app.innerHTML = `<section class="automa ${ai.id}"><header>${
     ai.name
-  }</header><div id="notes"><ul>${ai.notes
+  }</header><div class="notes"><ul>${ai.notes
     .map(n => `<li>${n}</li>`)
     .join(
       ''
-    )}</ul></div><article id="card"></article><div id="draw">Draw Card</div></section>`
-  const drawButton = document.getElementById('draw')
+    )}</ul></div><div class="controls"><div class="draw">Draw Card</div><div class="back">Back to Main</div></div><article id="card"></article></section>`
+  const drawButton = document.getElementsByClassName('draw')[0]
+  const backButton = document.getElementsByClassName('back')[0]
+  const backHandle = function(event) {
+    event.preventDefault()
+    if (window.confirm('Do you really want to go back to the ai selction?')) {
+      document.location.href = '/'
+    }
+  }
   const drawHandle = function(event) {
+    event.preventDefault()
     drawCard()
   }
   drawButton.addEventListener('click', drawHandle)
+  backButton.addEventListener('click', backHandle)
 }
 function loadChooseAutoma(data) {
-  const automaNames = data.automa.map(({ name }) => name)
+  const automaNames = data.automa.map(({ name, id }) => ({
+    name,
+    id
+  }))
   const app = document.getElementById('app')
   loadList()
   function deckSelect({ target }) {
     if (target.classList.contains('deck')) {
+      event.preventDefault()
       app.removeEventListener('click', deckSelect)
       loadDeck(target.dataset.id, data)
     }
   }
   function loadList() {
-    app.innerHTML = `<section><header>Choose Your Deck</header><ul>${automaNames
-      .map((name, i) => `<li class="deck" data-id=${i}>${name}</li>`)
+    app.innerHTML = `<section class="main"><header>Choose Your Deck</header><ul>${automaNames
+      .map(
+        ({ name, id }, i) => `<li class="deck ${id}" data-id=${i}>${name}</li>`
+      )
       .join('')}</ul></section>`
 
     app.addEventListener('click', deckSelect)
